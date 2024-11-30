242cbb200028b81b184b76097f9ad82e5b0cba61
Jake Carpenter <jake@jakecarpenter.dev>
2024-11-29 17:38:27 -0700

    Add tests for JSON data loading

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -1,10 +1,15 @@
-import builds from '$lib/data/builds.json'
 import {json} from '@sveltejs/kit'
 import type {Project} from '$lib/types'
 
 export async function GET() {
+  const builds = await readImportedBuildJson()
   const projects = builds.map<Project>(x => ({slug: x.project.slug, name: x.project.name}))
   const uniqueSlugs = Array.from(new Map(projects.map(item => [item.slug, item])).values())
   const sorted = uniqueSlugs.sort((a, b) => a.name.localeCompare(b.name))
   return json(sorted)
+}
+
+async function readImportedBuildJson() {
+  const imported = await import('$lib/data/builds.json')
+  return imported.default
 }
