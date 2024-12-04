8279119c3a1accdbd904d58a8be294033f8f59ed
Jake Carpenter <jake@jakecarpenter.dev>
2024-12-03 22:58:24 -0700

    Install vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vitest/config'
 import {sveltekit} from '@sveltejs/kit/vite'
 import {svelteTesting} from '@testing-library/svelte/vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [sveltekit(), svelteTesting()],
+  plugins: [sveltekit(), svelteTesting(), viteTsconfigPaths()],
   test: {
     environment: 'happy-dom',
     globals: true,
